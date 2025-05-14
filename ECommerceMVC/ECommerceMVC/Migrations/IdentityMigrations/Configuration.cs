9c2df751389f30fd82710983ae5233ecc5f42c4e
Yahiamostafa11 <yahiawork@gmail.com>
2025-05-14 20:47:13 +0300

    Adding new Taples for admin

---
+++ ECommerceMVC/ECommerceMVC/Migrations/IdentityMigrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace ECommerceMVC.Migrations.IdentityMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\IdentityMigrations";
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
